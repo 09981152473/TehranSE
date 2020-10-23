841972ef4289e2636dd3b9075943e51a0174512a
SinaMobasheri <sinamobasheri@outlook.com>
2020-10-23 12:01:42 +0330

    update to version 1.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 name = "xtehranse"
-version = "1.0.0"
+version = "1.1.0"
 description = "TehranSE is a python library to access Tehran Stock Exchange data"
 url = "https://github.com/SinaMobasheri/TehranSE"
 author = "SinaMobasheri"
